f839e0f71e56ad70045d729c6d117c5532a208ff
Raquel Canhoto <raquelcanhoto@Raquels-MBP.lan>
2023-11-19 17:26:33 +0000

    JS search engine and CSS updated

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+//Update the date
 let now = new Date();
 
 let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
@@ -24,3 +25,46 @@ currentMonth !== null && (currentMonth.innerHTML = months[now.getMonth()]);
 
 let currentDay = document.querySelector("#current-day");
 currentDay !== null && (currentDay.innerHTML = now.getDate());
+
+//Search engine
+let apiKey = "0t54a8o1afb5bf5b4dd0813b70dd9503";
+
+function newSearch(event) {
+  event.preventDefault();
+
+  let searchInput = document.querySelector("#input-search-form");
+  let valueInputInString = `${searchInput.value}`;
+  let apiURL = `https://api.shecodes.io/weather/v1/current?query=${valueInputInString}&key=${apiKey}&units=metric`;
+  let onlyLetters = /([a-zA-Z]+[\s]*)*/i;
+
+  function changeCity(response) {
+    let degrees = Math.round(response.data.temperature.current);
+    let changeDegrees = document.querySelector("#current-degree");
+
+    changeDegrees.innerHTML = `${degrees}º`;
+
+    let formattedString = valueInputInString.match(onlyLetters);
+
+    if (formattedString != null && valueInputInString != "") {
+      if (valueInputInString.length == formattedString[0].length) {
+        let city = document.querySelector("#city");
+
+        if (searchInput === null || city === null) {
+          return;
+        }
+
+        city.innerHTML = valueInputInString;
+        return;
+      }
+    }
+  }
+  axios
+    .get(apiURL)
+    .then(changeCity)
+    .catch(() => alert("Please, search only for the city name!"));
+
+  searchInput.value = null;
+}
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", newSearch);
